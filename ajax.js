6ea62dac6237e1f54ba1cdb44397104d10719832
ljh <cracker1023@gmail.com>
2022-08-30 14:53:03 +0900

    fetch연습

---
+++ ajax.js
@@ -0,0 +1,57 @@
+fetch('./test.json', {
+   method: 'GET',
+})
+   .then(res => res.json())
+   .then(data => console.log(data))
+   .catch(error => console.log('ERROR'));
+
+// test1
+// const getBtn = document.getElementById('getBtn');
+// const postBtn = document.getElementById('postBtn');
+
+// const getData = () => {
+//    const xhr = new XMLHttpRequest();
+//    xhr.open('GET', './todolist.html');
+
+//    xhr.onload = () => {
+//       console.log(xhr.response);
+//    };
+
+//    xhr.send();
+// };
+
+// const sendData = function () {};
+
+// getBtn.addEventListener('click', getData);
+// postBtn.addEventListener('click', postData);
+
+// test2
+// (function () {
+//    let httpReq;
+//    document.getElementById('ajaxBtn').addEventListener('click', makeReq);
+
+//    function makeReq() {
+//       httpReq = new XMLHttpRequest();
+//       if (!httpReq) {
+//          alert('XMLHTTP ERROR');
+//          return false;
+//       }
+//       httpReq.onreadystatechange = alertContents;
+//       httpReq.open('GET', 'test.html');
+//       httpReq.send();
+//    }
+
+//    function alertContents() {
+//       try {
+//          if (httpReq.readyState === XMLHttpRequest.DONE) {
+//             if (httpReq.status === 200) {
+//                alert(httpReq.responseText);
+//             } else {
+//                alert('req문제발생');
+//             }
+//          }
+//       } catch (e) {
+//          alert('Caught Exception' + e.descpirpttion);
+//       }
+//    }
+// });
--- todolist.js
+++ todolist.js
@@ -1,14 +1,57 @@
 'use strict';
 
+// ToDo list를 작성할 수 있는 폼
 const toDoForm = document.querySelector('.toDoForm');
+// ToDo list를 작성할 수 있는 인풋
 const toDoInput = toDoForm.querySelector('input');
+// ToDo list가 들어가있는 ul
 const toDos = document.querySelector('.toDos');
-const reBtn = document.createElement('button');
-const reBtnText = document.createTextNode('버튼생성');
-
-const TODOLIST = 'toDoList'; // 추가
+// 누르면 이름입력 할 수 있는 환영 버튼
+const welcomeBtn = document.createElement('button');
+// todolist를 배열에 담을 수 있는 함수
+const TODOLIST = 'toDoList';
 let toDoList = [];
 
+// 버튼 누르면 이름 입력창 나오게 하고 환영문구
+welcomeBtn.innerHTML = '환영버튼';
+document.body.appendChild(welcomeBtn);
+welcomeBtn.addEventListener('click', function () {
+   let name = prompt('What is your name?');
+   alert(`Hello ${name} let's go work!`);
+});
+
+// 새로운 버튼 생성하는 기능
+function newBtn() {
+   let btn = document.createElement('button');
+   btn.innerHTML = 'newnew!';
+   document.body.appendChild(btn);
+}
+
+// createToDo에서 인풋의 value 받으면 리스트에 받은 값을 새로 추가해주는 기능
+function paintToDo(toDo) {
+   const li = document.createElement('li');
+   const span = document.createElement('span');
+   const delBtn = document.createElement('button');
+   delBtn.innerText = '삭제';
+   delBtn.addEventListener('click', delToDo);
+   span.innerHTML = toDo;
+   li.appendChild(span);
+   toDos.appendChild(li);
+   li.appendChild(delBtn);
+   li.id = toDoList.length + 1;
+}
+
+// 삭제버튼으로 해당 리스트 삭제하고 로컬 스토리지에서도 삭제하기
+function delToDo(e) {
+   const button = e.target;
+   const li = button.parentNode;
+   toDos.removeChild(li);
+   toDoList = toDoList.filter(toDo => toDo.id !== Number(li.id));
+   localStorage.setItem(TODOLIST, JSON.stringify(toDoList));
+   console.log(toDoList);
+}
+
+// 로컬스토리지에서 리스트 받아오는 기능
 function loadToDoList() {
    const loadedToDoList = localStorage.getItem(TODOLIST);
    if (loadedToDoList !== null) {
@@ -20,28 +63,7 @@ function loadToDoList() {
       }
    }
 }
-
-// 새로운 버튼 생성하는 기능
-function newBtn() {
-   let btn = document.createElement('button');
-   btn.innerHTML = 'newnew!';
-   document.body.appendChild(btn);
-}
-
-// 버튼생성하는 버튼 만듦
-document.querySelector('.createBtn').addEventListener('click', function () {
-   newBtn();
-});
-
-// createToDo에서 인풋의 value 받으면 리스트에 받은 값을 새로 추가해주는 기능
-function paintToDo(toDo) {
-   const li = document.createElement('li');
-   const span = document.createElement('span');
-   span.innerHTML = toDo;
-   li.appendChild(span);
-   toDos.appendChild(li);
-}
-
+// 로컬스토리지에 목록 저장하는 기능(제이슨 형태로 풀어서 받기)
 function saveToDo(toDo) {
    const toDoObj = {
       text: toDo,
